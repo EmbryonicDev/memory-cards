9ac1e14ac3b6d04becbfba43b8e01709f0d0350d
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-29 20:01:30 +0200

    Cards.js: create shuffleCards()

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -48,8 +48,17 @@ export default function Cards() {
     return arr;
   }
 
+  const shuffledCards = arr => {
+    const newArr = arr.slice()
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+    }
+    return newArr
+  };
+
   return (
-    console.log(cardObjects())
+    console.log(shuffledCards(cardObjects()))
   )
 }
 
