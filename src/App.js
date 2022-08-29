df8f9644944c5a3ce6c3d17f055fb8af19edcdb4
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-29 20:15:32 +0200

    App: Set new state: gameLevel & pass to Cards as prop

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Header from "./components/Header";
 function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(localStorage.getItem('highScore') || 0);
-
-  console.log(Cards());
+  const [gameLevel, setGameLevel] = useState(0);
 
   return (
     <div className="App">
       <Header
         score={score}
         highScore={highScore}
+      />
+      <Cards
+        gameLevel={gameLevel}
       />
     </div>
   );
