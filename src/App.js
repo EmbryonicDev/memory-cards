5ca922c0491df1b4f35bd55a8d423b2034289af9
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-08-29 09:41:13 +0200

    Cards.js: Save work to continue on other computer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import Cards from "./components/Cards";
 import Header from "./components/Header";
 
 function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(localStorage.getItem('highScore') || 0);
+
+  console.log(Cards());
 
   return (
     <div className="App">
---
+++ src/components/Cards.js
@@ -0,0 +1,57 @@
+import apple from "../assets/icons/apple.svg"
+import bitcoin from "../assets/icons/bitcoin.svg"
+import brush from "../assets/icons/brush.svg"
+import bus from "../assets/icons/bus.svg"
+import camera from "../assets/icons/camera.svg"
+import car from "../assets/icons/car.svg"
+import crow from "../assets/icons/crow.svg"
+import dragon from "../assets/icons/dragon.svg"
+import feather from "../assets/icons/feather.svg"
+import fire from "../assets/icons/fire.svg"
+import hammer from "../assets/icons/hammer.svg"
+import hiker from "../assets/icons/hiker.svg"
+import hippo from "../assets/icons/hippo.svg"
+import horse from "../assets/icons/horse.svg"
+import igloo from "../assets/icons/igloo.svg"
+import meteor from "../assets/icons/meteor.svg"
+import moon from "../assets/icons/moon.svg"
+import motorbike from "../assets/icons/motorbike.svg"
+import robot from "../assets/icons/robot.svg"
+import shuttle from "../assets/icons/shuttle.svg"
+import snowman from "../assets/icons/snowman.svg"
+import tent from "../assets/icons/tent.svg"
+import gun from "../assets/icons/gun.svg"
+import worm from "../assets/icons/worm.svg"
+import { useState } from "react"
+
+export default function Cards() {
+  // Below arrays must match
+  const cardsIcons = [apple, bitcoin, brush, bus, camera, car, crow, dragon, feather, fire, hammer, hiker, hippo, horse, igloo, meteor, moon, motorbike, robot, shuttle, snowman, tent, gun, worm];
+  const draftCardTextArr = ['apple', 'bitcoin', 'brush', 'bus', 'camera', 'car', 'crow', 'dragon', 'feather', 'fire', 'hammer', 'hiker', 'hippo', 'horse', 'igloo', 'meteor', 'moon', 'motorbike', 'robot', 'shuttle', 'snowman', 'tent', 'gun', 'worm'];
+
+  function toTitleCase(arr) {
+    return arr.map(obj => obj.charAt(0).toUpperCase() + obj.substr(1))
+  }
+
+  const cardObjects = () => {
+    const cardTextArr = toTitleCase(draftCardTextArr);
+    const arr = [];
+
+    for (let i = 0; i < cardsIcons.length; i++) {
+      const obj = {
+        icon: cardsIcons[i],
+        text: cardTextArr[i]
+      };
+      arr.push(obj);
+    }
+    return arr;
+  }
+
+  return (
+    console.log(cardObjects())
+  )
+}
+
+
+
+
