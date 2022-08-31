c7f12c9ff579aeb81d7dc94d848c6a8eb06aaacb
djdevlam-apple <embryonic.dev@gmail.com>
2022-08-31 15:08:10 +0200

    DisplayCards: change bgc to blue on select for testing purposes

--- src/components/DisplayCards.js
+++ src/components/DisplayCards.js
@@ -1,10 +1,11 @@
 export default function DisplayCards(props) {
-  const { icon, text, handleClick } = props;
+  const { icon, text, handleClick, selected } = props;
 
   return (
     <div
       className="cardDiv"
       onClick={handleClick}
+      style={{ backgroundColor: selected && "blue" }}
     >
       <img src={icon} alt={text} />
       <h3>{text}</h3>
