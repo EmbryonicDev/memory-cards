f70bcdbf113095f00880a550c80dcb5c9b4544b2
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-09-02 17:50:12 +0200

    Modify DisplayCards to show card selections & wrong card if gameOver

--- src/components/DisplayCards.js
+++ src/components/DisplayCards.js
@@ -1,14 +1,30 @@
 export default function DisplayCards(props) {
-  const { icon, text, handleClick, selected } = props;
+  const { icon, text, handleClick, selected, gameOver, wrongCard } = props;
 
-  return (
-    <div
-      className="cardDiv"
-      onClick={handleClick}
-      style={{ backgroundColor: selected && "blue" }}
-    >
-      <img src={icon} alt={text} />
-      <h3>{text}</h3>
-    </div>
-  )
+  let style;
+
+  if (selected && gameOver) style = { backgroundColor: '#4ade80' };
+  if (text === wrongCard) style = { backgroundColor: 'red' };
+
+  if (gameOver) {
+    return (
+      <div
+        className="cardDiv"
+        style={style}
+      >
+        <img src={icon} alt={text} />
+        <h3>{text}</h3>
+      </div>
+    )
+  } else {
+    return (
+      <div
+        className="cardDiv"
+        onClick={handleClick}
+      >
+        <img src={icon} alt={text} />
+        <h3>{text}</h3>
+      </div>
+    )
+  }
 }
