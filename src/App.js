36347bb122794440f52d62b4413cd960c879cf7b
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-09-03 12:38:44 +0200

    App: Remove console log: new game

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [allCards, setAllCards] = useState(Cards());
   const [activeCards, setActiveCards] = useState(allCards.slice(0, 6));
   const [gameOver, setGameOver] = useState(false);
-  const [gameWon, setGameWon] = useState(false);
+  const [gameWon, setGameWon] = useState(true);
   const [wrongCard, setWrongCard] = useState();
   const [scoreIncrement, setScoreIncrement] = useState(1);
   const [selectedCards, setSelectedCards] = useState(0);
@@ -73,7 +73,6 @@ function App() {
   }
 
   function startNewGame() {
-    console.log('New Game')
     setActiveCards(allCards.slice(0, 6));
     score > highScore && setHighScore(score);
     setScore(0);
