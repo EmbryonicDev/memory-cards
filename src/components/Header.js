21d770069cebdb0dc8507f968468ea34e5cb8bb2
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-09-03 11:53:20 +0200

    Header: Add instructions in <ul>

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,15 @@ export default function Header(props) {
       <div className="textInfo">
         <h1 className="headerText">Memory Game</h1>
         <button>i</button>
+        <div className="instructions">
+          <ul>
+            <li>Each icon can be selected only once</li>
+            <li>After selecting all icons in level 1 you will progress to level 2</li>
+            <li>Each new level will contain all previously selected cards, as well as 6 new cards</li>
+            <li>As the game progresses, the score value for each selection will systematically increase</li>
+            <li>Complete all 4 levels to win the game</li>
+          </ul>
+        </div>
       </div>
       <div className="score">
         <h2>Current Score</h2>
